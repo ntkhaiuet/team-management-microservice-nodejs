8d999ffaff16a4a53b65f0b87d94dcfc8cef1b22
ntkhaiuet <ntkhaiuet@gmail.com>
2023-03-05 13:02:30 +0700

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 require("dotenv").config();
