1aaf6ad81b04440da8d9d2bce3f4ca78f3aae3af
ngoc.nguyenminh <ngoc.nguyenminh@amela.vn>
2023-03-28 22:01:59 +0700

    [feat] get list projects of user

--- routes/project.js
+++ routes/project.js
@@ -159,4 +159,119 @@ router.post("/create", verifyToken, async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Project:
+ *      required:
+ *        - _id
+ *        - name
+ *      properties:
+ *        _id:
+ *          type: String
+ *        name:
+ *          type: String
+ *        status:
+ *          type: String
+ *          enum: [Processing, Completed]
+ *          default: Processing
+ *        users:
+ *          type: Array
+ *          items:
+ *            type: object
+ *            properties:
+ *              user:
+ *                type: string
+ *              role:
+ *                type: string
+ *                enum: ["Leader", "Reviewer", "Member"]
+ *                default: "Member"
+ *        createdAt:
+ *          type: Date
+ *      example:
+ *        _id: 6422436f9574d6d0650f0059
+ *        name: Project cua Khai
+ *        status: Processing
+ *        users: [{user: "64106a4a65047e0dff8ecc81", role: "Leader"}]
+ *        createdAt: 2023-03-28T01:30:17.781+00:00
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Projects
+ *  description: Quản lý API Projects
+ */
+
+/**
+ * @swagger
+ * /api/project/list:
+ *  get:
+ *    summary: Lấy list project của người dùng
+ *    tags: [Projects]
+ *    security:
+ *      - bearerAuth: []
+ *    requestBody:
+ *      required: false
+ *    responses:
+ *      200:
+ *        description: Lấy ra danh sách thành công
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                success:
+ *                  default: true
+ *                message:
+ *                  default: Thay đổi mật khẩu thành công
+ *      400:
+ *        description: Thiếu trường bắt buộc/Email không tồn tại/Mật khẩu không đúng
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                success:
+ *                  default: false
+ *                message:
+ *                  default: Thiếu trường bắt buộc/Email không tồn tại/Mật khẩu không đúng
+ *      500:
+ *        description: Internal server error
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                success:
+ *                  default: false
+ *                message:
+ *                  default: Internal server error
+ */
+// @route GET api/project/list
+// @desc Lấy list project của người dùng
+// @access Private
+router.get("/list", verifyToken, async function (req, res) {
+  try {
+    const user = await User.findById(req.userId)
+      .populate("projects.project")
+      .exec();
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Không tìm thấy người dùng" });
+    }
+    let projects = user.projects;
+    res.json({
+      success: true,
+      message: "Lấy danh sách thành công",
+      data: projects
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
+
 module.exports = router;
