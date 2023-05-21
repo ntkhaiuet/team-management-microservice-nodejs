c332d7ec478d9631b2fcec56863434ea8428100d
ntkhaiuet <ntkhaiuet@gmail.com>
2023-05-21 18:58:27 +0700

    fix timezone UTC+7

--- middleware/formatDate.js
+++ middleware/formatDate.js
@@ -1,19 +1,3 @@
-// // Lấy ngày giờ hiện tại
-// const now = new Date(Date.now());
-
-// // Lấy các giá trị của ngày giờ và chuyển sang định dạng 2 chữ số nếu chỉ có 1 chữ số
-// const hours = String(now.getHours()).padStart(2, "0");
-// const minutes = String(now.getMinutes()).padStart(2, "0");
-// const seconds = String(now.getSeconds()).padStart(2, "0");
-// const day = String(now.getDate()).padStart(2, "0");
-// const month = String(now.getMonth() + 1).padStart(2, "0"); // Tháng bắt đầu từ 0, cộng thêm 1 để tính tháng từ 1 đến 12
-// const year = now.getFullYear();
-
-// // Chuyển sang định dạng HH:MM:SS DD/MM/YYYY
-// const formattedDate = `${hours}:${minutes}:${seconds} ${day}/${month}/${year}`;
-
-// module.exports = formattedDate;
-
 const moment = require("moment-timezone");
 
 const now = moment().tz("Asia/Ho_Chi_Minh"); // Lấy giờ hiện tại theo múi giờ UTC+7
--- middleware/onlyDate.js
+++ middleware/onlyDate.js
@@ -1,16 +1,3 @@
-// // Lấy ngày giờ hiện tại
-// const now = new Date(Date.now());
-
-// // Lấy các giá trị của ngày và chuyển sang định dạng 2 chữ số nếu chỉ có 1 chữ số
-// const day = String(now.getDate()).padStart(2, "0");
-// const month = String(now.getMonth() + 1).padStart(2, "0"); // Tháng bắt đầu từ 0, cộng thêm 1 để tính tháng từ 1 đến 12
-// const year = now.getFullYear();
-
-// // Chuyển sang định dạng DD/MM/YYYY
-// const onlyDate = `${day}/${month}/${year}`;
-
-// module.exports = onlyDate;
-
 const moment = require("moment-timezone");
 
 // Lấy giờ hiện tại theo múi giờ UTC+7
